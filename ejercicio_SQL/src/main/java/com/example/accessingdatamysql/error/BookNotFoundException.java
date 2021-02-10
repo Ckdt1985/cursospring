e75765593a69b3b4d923e022c7c6bb31d33d6118
Daniel Rojo <danielrojo@gmail.com>
2021-02-10 14:21:01 +0100

    ejercicio_SQL añadido

---
+++ ejercicio_SQL/src/main/java/com/example/accessingdatamysql/error/BookNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.accessingdatamysql;
+
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
